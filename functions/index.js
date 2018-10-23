af3de2610e99dab42d0575c3af9944560d85e605
Gunnar Hafdal <gunnar.hafdal@valtech.com>
2018-10-23 18:14:53 +0200

    saving before switching to typescript for functions

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,9 @@ const sheets = google.sheets('v4');
 
 const spreadsheetId = 'YOUR_SPREADSHEET_ID_HERE';
 
-const serviceAccount = require('../serviceAccount.json');
+//const serviceAccount = require('../serviceAccount.json');
 
-const jwtClient = new google.auth.JWT({
+/*const jwtClient = new google.auth.JWT({
     email: serviceAccount.client_email,
     key: serviceAccount.private_key,
     scopes: [ 'https://www.googleapis.com/auth/spreadsheets' ],  // read and write sheets
@@ -15,9 +15,8 @@ const jwtClient = new google.auth.JWT({
 
 const jwtAuthPromise = jwtClient.authorize();
 
-
-exports.copyPracticesToSheet = functions.database.ref('/users/DN3HZYxlkKeuoksqG9hVR8bWuxH3').onUpdate(async (change, context) => {
-    console.log(change, context);
+*/
+exports.copyPracticesToSheet = functions.database.ref('/users/DN3HZYxlkKeuoksqG9hVR8bWuxH3').onUpdate(async (change) => {
     const data = change.after.val();
     console.log(data);
     return
